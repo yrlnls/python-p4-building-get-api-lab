fd94ace521204a97675ad09c78ae0aba28d9d9db
tillenotuoma <totuoma27@gmail.com>
2025-04-06 20:14:22 +0300

    passing

--- server/app.py
+++ server/app.py
@@ -19,20 +19,27 @@ def index():
     return '<h1>Bakery GET API</h1>'
 
 @app.route('/bakeries')
-def bakeries():
-    return ''
+def get_bakeries():
+    bakeries = [bakery.to_dict() for bakery in Bakery.query.all()]
+    return make_response(  bakeries,   200  )
 
 @app.route('/bakeries/<int:id>')
-def bakery_by_id(id):
-    return ''
+def get_bakery_by_id(id):
+    bakery = Bakery.query.filter_by(id=id).first()
+    bakery_serialized = bakery.to_dict()
+    return make_response ( bakery_serialized, 200  )
+
 
 @app.route('/baked_goods/by_price')
-def baked_goods_by_price():
-    return ''
-
+def get_baked_goods_by_price():
+    baked_goods= BakedGood.query.order_by(BakedGood.price.desc()).all()
+    baked_goods_serialized = [baked_good.to_dict() for baked_good in baked_goods]
+    return make_response(baked_goods_serialized, 200)
 @app.route('/baked_goods/most_expensive')
-def most_expensive_baked_good():
-    return ''
+def get_most_expensive_baked_good():
+    baked_good = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    baked_good_serialized = baked_good.to_dict()
+    return make_response(baked_good_serialized, 200)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
